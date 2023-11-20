595d3ad08cb25a3ad109354948e1948e0aadacf5
Jocelyn-Bournier <bournierjocelyn@yahoo.fr>
2023-11-20 23:57:04 +0100

    remove bullshit

--- src/main/java/org/example/HelloAgent.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-import jade.core.Agent;
-public class HelloAgent extends Agent {
-    protected void setup() {
-        System.out.println("Hello world, I'm " + this.getLocalName());
-    }
-}
